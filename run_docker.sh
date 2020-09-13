79c157028048c6cf38f51272d138592c73b7f25a
Kumar Shanu Anand <kshanuanand5192@gmail.com>
2020-09-13 09:13:35 +0000

    application is running. added logging

--- run_docker.sh
+++ run_docker.sh
@@ -4,10 +4,10 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-sudo docker build -t kshanuanand/predictor:0.1 .
+docker build -t kshanuanand/predictor:0.1 .
 # Step 2: 
 # List docker images
-sudo docker image ls
+docker image ls
 # Step 3: 
 # Run flask app
-sudo docker run --rm -d kshanuanand/predictor:0.1  -p 8000:80
+docker run --rm -p  8000:80 kshanuanand/predictor:0.1
